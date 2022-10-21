2d0cc903e09d0d6f5efc8a29e38acbf3ec7c1ebd
Varshini <varshuk01@gmail.com>
2022-10-21 15:51:59 +0530

    tweet

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-signin',
@@ -7,7 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myRouter:Router,private myapi:ApiService) { }
+  emailid=""
+  password=""
+
+  readValues=()=>{
+let data={
+  "emailid":this.emailid,
+  "password":this.password
+}
+
+
+
+
+console.log(data)
+this.myapi.addData(data).subscribe(
+  (response:any)=>{
+
+    if(response.length>0){
+      localStorage.setItem("stored_name",response[0].name)
+      localStorage.setItem("stored_id",response[0].id)
+      this.myRouter.navigate(["/tweet"])
+    }
+    else{
+      alert("invalid")
+    }
+  }
+)
+  }
 
   ngOnInit(): void {
   }
